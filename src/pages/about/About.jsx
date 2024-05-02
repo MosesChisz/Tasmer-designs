c38ed2d4b1f5c59effc57e5a92b77668a02392c7
chimbo87 <archfordnhanga@gmail.com>
2024-05-02 11:59:12 +0200

    commit all

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import homeaboutbanner from "../../assets/aboutbanner02.png";
+import "./About.css";
+import Footer from "../../components/footer/Footer";
+
 function About() {
   return (
     <>
@@ -56,83 +59,262 @@ function About() {
             </div>
           </div>
         </div>
-
-      
       </div>
       <div className="container-fluid" id="aboutServices">
         <div className="container" id="aboutServicesText">
-        <h2>What We Do<span></span></h2>
-        <div class="row">
-        
-          <div class="col-lg-4 col-md-4">
-            <div id="ourServiceCard">
-              <h6>Male Dressing</h6>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown
-              </p>
-              <button>Read More</button>
+          <h2>
+            What We Do<span></span>
+          </h2>
+          <div class="row">
+            <div class="col-lg-4 col-md-4">
+              <div id="ourServiceCard">
+                <h6>Male Dressing</h6>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                </p>
+                <button>Read More</button>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div id="ourServiceCard">
+                <h6>Male Dressing</h6>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                </p>
+                <button>Read More</button>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div id="ourServiceCard">
+                <h6>Male Dressing</h6>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                </p>
+                <button>Read More</button>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div id="ourServiceCard">
+                <h6>Male Dressing</h6>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                </p>
+                <button>Read More</button>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div id="ourServiceCard">
+                <h6>Male Dressing</h6>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                </p>
+                <button>Read More</button>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div id="ourServiceCard">
+                <h6>Male Dressing</h6>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                </p>
+                <button>Read More</button>
+              </div>
             </div>
           </div>
-          <div class="col-lg-4 col-md-4">
-            <div id="ourServiceCard">
-              <h6>Male Dressing</h6>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown
-              </p>
-              <button>Read More</button>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-4">
-            <div id="ourServiceCard">
-              <h6>Male Dressing</h6>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown
-              </p>
-              <button>Read More</button>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-4">
-            <div id="ourServiceCard">
-              <h6>Male Dressing</h6>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown
-              </p>
-              <button>Read More</button>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-4">
-            <div id="ourServiceCard">
-              <h6>Male Dressing</h6>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown
-              </p>
-              <button>Read More</button>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-4">
-            <div id="ourServiceCard">
-              <h6>Male Dressing</h6>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown
-              </p>
-              <button>Read More</button>
-            </div>
-          </div>
-        </div>
         </div>
       </div>
+      <div className="container" id="aboutAccodion">
+        <h4>Why Choose Us ?</h4>
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                Accordion Item #1
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              class="accordion-collapse collapse show"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <strong>This is the first item's accordion body.</strong> It is
+                shown by default, until the collapse plugin adds the appropriate
+                classes that we use to style each element. These classes control
+                the overall appearance, as well as the showing and hiding via
+                CSS transitions. You can modify any of this with custom CSS or
+                overriding our default variables. It's also worth noting that
+                just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                Accordion Item #2
+              </button>
+            </h2>
+            <div
+              id="collapseTwo"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <strong>This is the second item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+              >
+                Accordion Item #3
+              </button>
+            </h2>
+            <div
+              id="collapseThree"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseFour"
+                aria-expanded="false"
+                aria-controls="collapseFour"
+              >
+                Accordion Item #4
+              </button>
+            </h2>
+            <div
+              id="collapseFour"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseFive"
+                aria-expanded="false"
+                aria-controls="collapseFive"
+              >
+                Accordion Item #5
+              </button>
+            </h2>
+            <div
+              id="collapseFive"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid" id="ourAboutStory">
+        <div className="container">
+          <h4>Our Story</h4>
+          <iframe
+            width="100%"
+            height="450"
+            borderRadius="4"
+            src="https://www.youtube.com/embed/e-HZ2TIVULI?si=g7VUCtM18FM3zafs"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
--- src/pages/fashion/Fashion.jsx
+++ src/pages/fashion/Fashion.jsx
@@ -1,28 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import homebanner from "../../assets/homebanner05.png";
+import "./Fashion.css";
 
 function Fashion() {
+  const [activeButton, setActiveButton] = useState(null);
+  const handleButtonClick = (buttonId) => {
+    setActiveButton(buttonId);
+  };
   return (
-    <div>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and
-        typesetting industry. Lorem Ipsum has been the industry's standard dummy
-        text ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to make a type specimen book. It has survived not only
-        five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum
-      </p>
-    </div>
+    <>
+      <div id="theFashionPage">
+        <h1>Fashion</h1>
+      </div>
+      <div className="container" id="ourWorkBtnsBox">
+        <div id="ourWorkBtns">
+          <div>
+            <button
+              className={activeButton === 1 ? "active" : ""}
+              onClick={() => handleButtonClick(1)}
+            >
+              All
+            </button>
+          </div>
+          <div>
+            <button
+              className={activeButton === 2 ? "active" : ""}
+              onClick={() => handleButtonClick(2)}
+            >
+              Male Fashion
+            </button>
+          </div>
+
+          <div>
+            <button
+              className={activeButton === 3 ? "active" : ""}
+              onClick={() => handleButtonClick(3)}
+            >
+              Female Fashion
+            </button>
+          </div>
+
+          <div>
+            <button
+              className={activeButton === 4 ? "active" : ""}
+              onClick={() => handleButtonClick(4)}
+            >
+              Kids Fashion
+            </button>
+          </div>
+
+          <div>
+            <button
+              className={activeButton === 5 ? "active" : ""}
+              onClick={() => handleButtonClick(5)}
+            >
+              Wedding
+            </button>
+          </div>
+
+          <div>
+            <button
+              className={activeButton === 6 ? "active" : ""}
+              onClick={() => handleButtonClick(6)}
+            >
+              Wedding
+            </button>
+          </div>
+
+          <div>
+            <button
+              className={activeButton === 7 ? "active" : ""}
+              onClick={() => handleButtonClick(7)}
+            >
+              Wedding
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-4">
+            <div id="fashionCard">
+              <img src={homebanner} alt="" />
+              <p>Ladies African Attire</p>
+              <div>
+                <button>Order Now</button>
+                <button>View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
